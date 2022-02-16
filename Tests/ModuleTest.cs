91501bffee7b7d15ea260eb91d6c4e2bf47f1a39
Dan <Danhirst123@gmail.com>
2022-02-16 13:13:07 +0000

    unit tests

---
+++ Tests/ModuleTest.cs
@@ -0,0 +1,47 @@
+﻿using FaceAttendance.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaceAttendance.Tests
+{
+    [TestFixture]
+    public class ModuleTest
+    {
+        [Test]
+        public void CheckID()
+        {
+
+            Module m = new Module
+            {
+                ID = 1
+            };
+
+            Assert.AreEqual(1, m.ID);
+        }
+        [Test]
+        public void CheckModuleName()
+        {
+
+            Module m = new Module
+            {
+                ModuleName = "Comp"
+            };
+
+            Assert.AreEqual("Comp",m.ModuleName);
+        }
+        [Test]
+        public void CheckModuleCode()
+        {
+
+            Module m = new Module
+            {
+                ModuleCode = "Comp 3000"
+            };
+
+            Assert.AreEqual("Comp 3000", m.ModuleCode);
+        }
+    }
+}
--- Tests/StudentTest.cs
+++ Tests/StudentTest.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FaceAttendance.Models;
 using NUnit.Framework;
 
 namespace FaceAttendance.Tests
@@ -11,10 +12,61 @@ namespace FaceAttendance.Tests
     {
 
         [Test]
-        public void IsPrime_InputIs1_ReturnFalse()
+        public void CheckID()
         {
 
-            Assert.AreEqual("1", "1");
+            Student s = new Student
+            {
+                ID = 1
+            };
+
+            Assert.AreEqual(1, s.ID);
+        }
+
+        [Test]
+        public void CheckStudentCode()
+        {
+
+            Student s = new Student
+            {
+                StudentCode = 2
+            };
+
+            Assert.AreEqual(2, s.StudentCode);
+        }
+
+        [Test]
+        public void CheckStudentName()
+        {
+
+            Student s = new Student
+            {
+                StudentName = "steve"
+            };
+
+            Assert.AreEqual("steve", s.StudentName);
+        }
+        [Test]
+        public void CheckImageURL()
+        {
+
+            Student s = new Student
+            {
+                imageUrl = "www.google.com"
+            };
+
+            Assert.AreEqual("www.google.com", s.imageUrl);
+        }
+        [Test]
+        public void active()
+        {
+
+            Student s = new Student
+            {
+                active = true
+            };
+
+            Assert.AreEqual(true, s.active);
         }
     }
 }
